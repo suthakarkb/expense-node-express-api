505f062b6e92cc5a83b77e2d1ed885a997eb7320
Suthakar Bose <suthakarkb@gmail.com>
2021-05-27 23:02:48 +0800

    Vulnerabilities resolved

--- config/config.js
+++ config/config.js
@@ -19,23 +19,23 @@ config.auth = false;
 config.dbhost= '127.0.0.1';
 config.dialect= 'mysql';
 config.database = 'expense';
-config.username = 'root';
-config.password = 'root1234';
+config.username = '';
+config.password = '';
 config.table_prefix = '';
-config.secretKey = 'SecretSG50';
-config.authKey = "key=AAAA3iU9_BI:APA91bEtxGrrpTp2q00B6Q9QjY5EWLW63PlRuPsC_CVkjXAej6c3uBYNpssnvscRslzX6sEEOKr5cBxQz0vNmVwd1xTPg2rco7dlqdzb2-zOsdoMfwDqHj3e8FgCEjLi4pMKtkqIEzVN";
-config.salt = "F3229A0B371ED2D9441B830D21A390C3";
+config.secretKey = '';
+config.authKey = "";
+config.salt = "";
 //Pagination
 config.paginate = true;
 config.page_limit = 10;
 
 //smtp server
-config.smtphost = 'mail.appseries.net';
+config.smtphost = '';
 config.smtpport = 465;
 // not secured smtpport = 587, secured smtp port = 465
-config.mailfrom = 'support@appseries.net';
-config.authuser = 'support@appseries.net';
-config.authpass = '5lm@,=cA#^Jl';
+config.mailfrom = '';
+config.authuser = '';
+config.authpass = '';
 //Configuration of mysql connections
 const pool = mysql.createPool({
 	connectionLimit: 20,
